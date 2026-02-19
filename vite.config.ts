1d04c94a890728ce032f417a9b87a6fb9355c30f
SandrinePichla <sandrinepichla@hotmail.com>
2026-02-19 18:35:08 +0100

    gh-pages-deploiement

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/martial-arts-hub/",
+   base: "/amsp-website/",
   server: {
     host: "::",
     port: 8080,
