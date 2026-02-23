583e09897946230e73619c699eeece1bdbdc7e9d
SandrinePichla <sandrinepichla@hotmail.com>
2026-02-23 19:55:22 +0100

    Connexion à Sanity

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/martial-arts-hub">
+      <BrowserRouter basename="/amsp-website">
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/disciplines" element={<Disciplines />} />
